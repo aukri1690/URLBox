42d5e84ef2dba7237f8bf913a99015cfdfe879e5
aukri1690 <toro369@icloud.com>
2025-08-05 16:32:12 +0900

    feat: implement ColorModeButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client"
+
+import React from 'react'
+import { ColorModeButton } from "@/components/ui/color-mode"
+
+
 const Home = () => {
   return(
-    <h2>Hello URLBox</h2>
+    <>
+    <ColorModeButton />
+    </>
   )
 }
 
